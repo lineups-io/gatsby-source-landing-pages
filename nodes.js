0916292e9031ed25995d672743173bdc7b097e3a
Lucas Blancas <lablancas@gmail.com>
2019-08-06 10:38:54 -0500

    Revert "export generate type name helper function and type names"
    This reverts commit 25cb7815f7d54ce6f7b78494336fd430c404e997.

--- nodes.js
+++ nodes.js
@@ -1,20 +1,9 @@
 const createNodeHelpers = require('gatsby-node-helpers').default
 
-const {
-  createNodeFactory,
-  generateTypeName,
-} = createNodeHelpers({ typePrefix: 'Lineups' })
+const { createNodeFactory } = createNodeHelpers({ typePrefix: 'Lineups' })
 
-exports.ApartmentType = 'Apartment'
-exports.ImageType = 'Image'
-exports.MarketType = 'Market'
-exports.PageType = 'Page'
-exports.SiteType = 'Site'
-
-exports.generateTypeName = generateTypeName
-
-exports.ApartmentNode = createNodeFactory(ApartmentType)
-exports.ImageNode = createNodeFactory(ImageType)
-exports.MarketNode = createNodeFactory(MarketType)
-exports.PageNode = createNodeFactory(PageType)
-exports.SiteNode = createNodeFactory(SiteType)
+exports.ApartmentNode = createNodeFactory('Apartment')
+exports.ImageNode = createNodeFactory('Image')
+exports.MarketNode = createNodeFactory('Market')
+exports.PageNode = createNodeFactory('Page')
+exports.SiteNode = createNodeFactory('Site')
