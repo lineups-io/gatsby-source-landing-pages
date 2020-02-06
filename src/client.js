e5ece19832cedc6b6ad7a3f1fad70bc8f8f8c792
Lucas Blancas <lablancas@gmail.com>
2020-02-06 13:06:42 -0600

    remove async - createLink and headers as functions not supported

--- src/client.js
+++ src/client.js
@@ -3,26 +3,20 @@ const { createHttpLink } = require('apollo-link-http')
 const { InMemoryCache } = require('apollo-cache-inmemory')
 const nodeFetch = require('node-fetch')
 
-module.exports = async options => {
+module.exports = options => {
   const {
     url,
     headers = {},
     fetch = nodeFetch,
     fetchOptions = {},
-    createLink,
   } = options
 
-  let link
-  if (createLink) {
-    link = await createLink(options)
-  } else {
-    link = createHttpLink({
-      uri: url,
-      fetch,
-      fetchOptions,
-      headers: typeof headers === 'function' ? await headers() : headers,
-    })
-  }
+  const link = createHttpLink({
+    uri: url,
+    fetch,
+    fetchOptions,
+    headers,
+  })
 
   return new ApolloClient({
     link,
